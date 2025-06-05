7cf1bb1da487837a72eb0cb0336d6164e4e9c220
cine10tv <57457353+kadanadam@users.noreply.github.com>
2025-06-05 23:30:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_canli_tv_m3u():
         print(f"✅ {len(channels)} kanal bulundu")
         
         
-        with open("yeni.m3u", "w", encoding="utf-8") as f:
+        with open("app.m3u", "w", encoding="utf-8") as f:
             f.write("#EXTM3U\n")
             
             kanal_sayisi = 0
@@ -64,7 +64,7 @@ def get_canli_tv_m3u():
                 f.write(f'{hls_url}\n')
                 kanal_sayisi += 1
         
-        print(f"📺 yeni.m3u dosyası oluşturuldu! ({kanal_sayisi} kanal)")
+        print(f"📺 app.m3u dosyası oluşturuldu! ({kanal_sayisi} kanal)")
         return True
         
     except Exception as e:
